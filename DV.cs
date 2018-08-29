d65ec3b87200d7f7809f87daf86a09fe9fb32cf7
gusyang <gus.yang@aol.com>
2018-08-28 22:08:47 -0700

    C# tool

---
+++ DV.cs
@@ -0,0 +1,46 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using consolJson;
+//
+//    var dv = Dv.FromJson(jsonString);
+
+namespace consolJson
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Dv
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Value")]
+        public string Value { get; set; }
+    }
+
+    public partial class Dv
+    {
+        public static List<Dv> FromJson(string json) => JsonConvert.DeserializeObject<List<Dv>>(json, consolJson.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this List<Dv> self) => JsonConvert.SerializeObject(self, consolJson.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace consolJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.AppSettings["conn"]))
+            {
+                SqlCommand com1 = new SqlCommand(ConfigurationManager.AppSettings["com1"]);
+                com1.CommandType = CommandType.Text;
+                com1.Connection = conn;
+                conn.Open();
+                SqlDataReader reader = com1.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string delivery = "";
+                        string json = reader.GetString(1);
+                        List<Dv> dvs = Dv.FromJson(json);
+                        if (dvs.Count > 0)
+                        {
+                            foreach(Dv dv in dvs)
+                            {
+                                if (dv.Name.Equals("#DV_DeliveryDate#", StringComparison.CurrentCultureIgnoreCase))
+                                    delivery = dv.Value.Substring(15, dv.Value.Length - 15);
+                                DateTime tmp;
+                                if (DateTime.TryParse(delivery, out tmp))
+                                {
+                                    SqlCommand update = new SqlCommand(ConfigurationManager.AppSettings["com2"]);
+                                    update.CommandType = CommandType.Text;
+                                    update.Connection = conn;
+                                    SqlParameter pid = new SqlParameter();
+                                    pid.ParameterName = "@ID";
+                                    pid.SqlDbType = SqlDbType.Int;
+                                    pid.Direction = ParameterDirection.Input;
+                                    pid.Value = id;
+
+
+                                    SqlParameter deliveryValue = new SqlParameter();
+                                    deliveryValue.ParameterName = "@DeliveryTime";
+                                    deliveryValue.SqlDbType = SqlDbType.DateTime;
+                                    deliveryValue.Direction = ParameterDirection.Input;
+                                    deliveryValue.Value = delivery;
+
+                                    update.Parameters.Add(pid);
+                                    update.Parameters.Add(deliveryValue);
+                                    update.ExecuteNonQuery();                                    
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                reader.Close();
+                if(conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                Console.Read();
+            }
+
+        }
+    }
+}
